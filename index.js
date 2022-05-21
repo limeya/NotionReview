a9d0d5da093f64ea1a9d2056ba533e6ad949bf57
Li Mengyang <94220657+limeya@users.noreply.github.com>
2022-05-21 18:00:55 +0800

    use env variables to import secrets

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const { Client } = require('@notionhq/client');
-const core = require("@actions/core")
+// const core = require("@actions/core")
 
-const notion_api_key = core.getInput("notion_api_key", { required: true })
-const database_id = core.getInput("database_id", { required: true })
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+// const notion_api_key = core.getInput("notion_api_key", { required: true })
+// const database_id = core.getInput("database_id", { required: true })
 
 const notion = new Client({ auth: notion_api_key });
 
 (async () => {
+  const databaseId = process.env.DATABASE_ID;
   const response = await notion.databases.query({
-    database_id: database_id,
+    database_id: databaseId,
     filter: {
       and: [
         {
           property: '状态',
           select: {
-            equals: '已总结',
+            is_empty: true,
           },
         }
       ],

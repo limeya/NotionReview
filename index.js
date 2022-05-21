d7b0fb5c38f3132b0436528cd04389716cceb796
Li Mengyang <94220657+limeya@users.noreply.github.com>
2022-05-21 17:50:17 +0800

    use actions/core get secrets

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const { Client } = require('@notionhq/client');
+const core = require("@actions/core")
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const notion_api_key = core.getInput("notion_api_key", { required: true })
+const database_id = core.getInput("database_id", { required: true })
+
+const notion = new Client({ auth: notion_api_key });
 
 (async () => {
-  const databaseId = process.env.ARTICLE_DATABASE_ID;
   const response = await notion.databases.query({
-    database_id: databaseId,
+    database_id: database_id,
     filter: {
       and: [
         {
